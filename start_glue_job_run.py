6baa0f175eebcb503c29edb308a7b78c3c1c63f8
Otto Jolanki <ojolanki@stanford.edu>
2023-10-10 10:50:39 -0700

    job starter script

---
+++ start_glue_job_run.py
@@ -0,0 +1,33 @@
+import argparse
+import boto3
+
+def get_glue_client(profile_name, region_name):
+    session = boto3.session(profile_name=profile_name, region_name=region_name)
+    client = session.client('glue')
+    return client
+
+def make_glue_job_args(input_bucket_name, output_bucket_name, log_date_prefix, output_partition_count):
+    glue_job_args = {
+        '--input_bucket_name': input_bucket_name,
+        '--output_bucket_name': output_bucket_name,
+        '--log_date_prefix': log_date_prefix,
+        '--output_partition_count': output_partition_count,
+    }
+    return glue_job_args
+
+def main(args):
+    client = get_glue_client(args.profile_name, args.region_name)
+    glue_args = make_glue_job_args(args.input_bucket_name, args.output_bucket_name, argsl.log_date_prefix, args.output_partition_count)
+    client.start_jonb_run(JobName=args.glue_job_name, Arguments=glue_args)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--profile-name', type=str, required=True)
+    parser.add_argument('--region-name', type=str, required=True)
+    parser.add_argument('--input-bucket-name', type=str, required=True)
+    parser.add_argument('--output-bucket-name', type=str, required=True)
+    parser.add_argument('--log-date-prefix', type=str, required=True)
+    parser.add_argument('--glue-job-name', type=str, required=True)
+    parser.add_argument('--output-partition-count', type=int, default=48)
+    args = parser.parse_args()
+    main(args)
